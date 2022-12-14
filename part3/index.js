2b2118cc8d7298eb6bc088e21b8be134fef8080b
Taina <kankainen.taina@gmail.com>
2022-12-14 23:32:06 +0200

    3.3 & 3.4

--- part3/index.js
+++ part3/index.js
@@ -28,13 +28,31 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`Phonebook has info for ${persons.length} people <br><br> ${date}`)
+})
+
 app.get('/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get('/info', (request, response) => {
-    const date = new Date()
-    response.send(`Phonebook has info for ${persons.length} people <br><br> ${date}`)
+app.get('/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+})
+
+app.delete('/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
 })
 
 const PORT = 3001
