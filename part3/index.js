1a7fc698ea860e942828a4aa37b5a0e81881c68b
Taina <kankainen.taina@gmail.com>
2022-12-15 13:23:08 +0200

    3.13 & 3.14

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-
 const cors = require('cors')
+const Person = require('./models/person')
 
 var morgan = require('morgan')
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
@@ -28,7 +29,7 @@ let persons = [
         id: 4
     }
 ]
-
+  
 app.use(express.json())
 
 app.use(express.static('build'))
@@ -37,41 +38,14 @@ app.use(cors())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
     const date = new Date()
     response.send(`Phonebook has info for ${persons.length} people <br><br> ${date}`)
 })
-
-app.get('/persons', (request, response) => {
-    response.json(persons)
-})
-
-app.get('/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-})
-
-app.delete('/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-})
-
-const generateId = () => {
-    const randomId = Math.floor(Math.random() * 100000);
-    return randomId
-}
 
 app.post('/persons', (request, response) => {
     const body = request.body
@@ -90,18 +64,37 @@ app.post('/persons', (request, response) => {
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id: generateId()
-    }
+        number: body.number
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        console.log(person)
+        response.json(savedPerson)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+app.get('/persons', (request, response) => {
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+})
+
+app.get('/persons/:id', (request, response) => {
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
+})
+
+app.delete('/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
